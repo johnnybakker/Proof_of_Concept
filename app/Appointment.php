cd7b30bc50eda7ae16f48e50d258a1a5f1c1e6d4
Arjan de Coninck <arjan.de.coninck@triglobal.org>
2018-06-12 20:23:24 +0200

    change validation

--- app/Appointment.php
+++ app/Appointment.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     protected $fillable = ['user_id', 'title', 'start_datetime', 'end_datetime', 'zipcode', 'address', 'city', 'description', 'created_at', 'updated_at'];
+
+    public function rules() {
+        $rules = [
+            'title' => 'required',
+            'start_datetime' => 'required',
+            'end_datetime' => 'required',
+            'zipcode' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'description' => 'required'
+        ];
+
+        return $rules;
+    }  
 }
--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Appointment;
 use \Auth;
 use Illuminate\Http\Request;
+use Validator;
+
 
 class AppointmentController extends Controller
 {
@@ -25,27 +27,30 @@ class AppointmentController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required',
-            'start_datetime' => 'required',
-            'end_datetime' => 'required',
-            'zipcode' => 'required',
-            'address' => 'required',
-            'city' => 'required'
-        ]);
-        
         $appointment = new Appointment();
-        $appointment->user_id = Auth::id();
-        $appointment->title = $request->get('title');
-        $appointment->start_datetime = $request->get('start_datetime');
-        $appointment->end_datetime = $request->get('end_datetime');
-        $appointment->zipcode = $request->get('zipcode');
-        $appointment->address = $request->get('address');
-        $appointment->city = $request->get('city');
-        $appointment->description = $request->get('description');
-        $appointment->save();
 
-        return redirect(route('appointments.index'));
+        $validator = Validator::make($request->all(), $appointment->rules());
+
+        if ($validator->fails()) {
+            return redirect(route('appointments.create'))
+                ->withErrors($validator)
+                ->withInput();
+        }
+        else {            
+            $appointment->user_id = Auth::id();
+            $appointment->title = $request->get('title');
+            $appointment->start_datetime = $request->get('start_datetime');
+            $appointment->end_datetime = $request->get('end_datetime');
+            $appointment->zipcode = $request->get('zipcode');
+            $appointment->address = $request->get('address');
+            $appointment->city = $request->get('city');
+            $appointment->description = $request->get('description');
+            $appointment->save();
+
+            return redirect(route('appointments.index'));
+        }
+
+        
     }
 
     public function show(Appointment $appointment)
